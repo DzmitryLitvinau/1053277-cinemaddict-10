ac90c9b7eb08a76670fb50383dc326966954e290
DzmitryLitvinau <angeridze93@gmail.com>
2019-11-24 01:19:35 +0300

    Add board.js

---
+++ src/components/board.js
@@ -0,0 +1,20 @@
+export const createBoardTemplate = () => {
+  return (
+    `<section class="films">
+    <section class="films-list">
+      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+      <div class="films-list__container js-main-films__container"></div>
+    </section>
+    <section class="films-list--extra">
+      <h2 class="films-list__title">Top rated</h2>
+
+      <div class="films-list__container js-rated-films__container"></div>
+    </section>
+    <section class="films-list--extra">
+      <h2 class="films-list__title">Most commented</h2>
+
+      <div class="films-list__container js-commented-films__container"></div>
+    </section>
+    </section>`
+  );
+};
