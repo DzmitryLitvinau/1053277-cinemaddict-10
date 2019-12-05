3a6e2615680beb6a8499517baf6b787b1ade446f
DzmitryLitvinau <angeridze93@gmail.com>
2019-12-06 01:46:02 +0300

    Replace fish data with real in main-navigation.js

--- src/components/main-navigation.js
+++ src/components/main-navigation.js
@@ -1,11 +1,18 @@
-export const createMainNavigationTemplate = () => {
+const createFilterMarkup = (filter) => {
+  const { name, count } = filter;
+
+  return (
+    `<a href="#${name}" class="main-navigation__item">${name} <span class="main-navigation__item-count">${count}</span></a>`
+  );
+};
+
+export const createMainNavigationTemplate = (filters) => {
+  const filtersMarkup = filters.map((it, i) => createFilterMarkup(it, i === 0)).join(`\n`);
+
   return (
     `<nav class="main-navigation">
-  <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-  <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-  <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-  <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
-  <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
-</nav>`
+      ${filtersMarkup}
+      <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+    </nav>`
   );
 };
