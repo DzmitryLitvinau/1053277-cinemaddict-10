96f3bd68134e0ca1f38ecfac60758d50e2d7689a
DzmitryLitvinau <angeridze93@gmail.com>
2019-12-06 01:46:34 +0300

    Replace fish data with real in film-details-popup

--- src/components/film-details-popup.js
+++ src/components/film-details-popup.js
@@ -1,4 +1,16 @@
-export const createFilmDetailsPopupTemplate = () => {
+import { monthNames } from '../consts.js';
+import { createCommentsTemplate } from "./comments.js";
+import { generateComments } from "../mock/comments.js";
+
+export const createFilmDetailsPopupTemplate = (card) => {
+  const COMMENTS_COUNT = 5;
+  const commentsCount = generateComments(COMMENTS_COUNT);
+  const commentsTemplate = createCommentsTemplate(commentsCount);
+
+  const { title, poster, age, director, writers, actors, country, rating, description, year, date, duration, genre, comments } = card;
+  const writersString = writers.slice(0, -2);
+  const actorsString = actors.slice(0, -2);
+  const getRandomDateAndYear = `${date.getDate()} ${monthNames[date.getMonth()]} ${year}`;
   return (
     `<section class="film-details js-film-details">
   <form class="film-details__inner" action="" method="get">
@@ -8,59 +20,59 @@ export const createFilmDetailsPopupTemplate = () => {
       </div>
       <div class="film-details__info-wrap">
         <div class="film-details__poster">
-          <img class="film-details__poster-img" src="./images/posters/the-great-flamarion.jpg" alt="">
+          <img class="film-details__poster-img" src="${poster}" alt="">
 
-          <p class="film-details__age">18+</p>
+          <p class="film-details__age">${age}+</p>
         </div>
 
         <div class="film-details__info">
           <div class="film-details__info-head">
             <div class="film-details__title-wrap">
-              <h3 class="film-details__title">The Great Flamarion</h3>
-              <p class="film-details__title-original">Original: The Great Flamarion</p>
+              <h3 class="film-details__title">${title}</h3>
+              <p class="film-details__title-original">Original: ${title}</p>
             </div>
 
             <div class="film-details__rating">
-              <p class="film-details__total-rating">8.9</p>
+              <p class="film-details__total-rating">${rating}</p>
             </div>
           </div>
 
           <table class="film-details__table">
             <tr class="film-details__row">
               <td class="film-details__term">Director</td>
-              <td class="film-details__cell">Anthony Mann</td>
+              <td class="film-details__cell">${director}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Writers</td>
-              <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+              <td class="film-details__cell">${writersString}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Actors</td>
-              <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+              <td class="film-details__cell">${actorsString}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Release Date</td>
-              <td class="film-details__cell">30 March 1945</td>
+              <td class="film-details__cell">${getRandomDateAndYear}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Runtime</td>
-              <td class="film-details__cell">1h 18m</td>
+              <td class="film-details__cell">${duration}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Country</td>
-              <td class="film-details__cell">USA</td>
+              <td class="film-details__cell">${country}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Genres</td>
               <td class="film-details__cell">
-                <span class="film-details__genre">Drama</span>
-                <span class="film-details__genre">Film-Noir</span>
-                <span class="film-details__genre">Mystery</span></td>
+                <span class="film-details__genre">${genre}</span>
+                <span class="film-details__genre">${genre}</span>
+                <span class="film-details__genre">${genre}</span></td>
             </tr>
           </table>
 
           <p class="film-details__film-description">
-            The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
+          ${description}
           </p>
         </div>
       </div>
@@ -79,10 +91,8 @@ export const createFilmDetailsPopupTemplate = () => {
 
     <div class="form-details__bottom-container">
       <section class="film-details__comments-wrap">
-        <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">0</span></h3>
-
-        <ul class="film-details__comments-list"></ul>
-
+         <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments}</span></h3>
+            ${commentsTemplate}
         <div class="film-details__new-comment">
           <div for="add-emoji" class="film-details__add-emoji-label">
             <img src="images/emoji/smile.png" width="55" height="55" alt="emoji">
